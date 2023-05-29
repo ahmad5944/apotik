7697b9f88dd1bafa70bed186915e75d86b6a3b51
Ahmad Daffa Dhaifullah <47172711+ahmad5944@users.noreply.github.com>
2023-05-30 01:23:07 +0700

    test

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -55,7 +55,7 @@ class LaporanController extends Controller
     {
         $periode=$request->get('periode');
         $jenis=$request->get('jenis');
-        // 
+        // s
         if($jenis=='cetak') {
             if($periode == 'All') {
                 $bb = \App\Laporan::All();
